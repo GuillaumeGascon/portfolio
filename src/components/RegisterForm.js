27fbb5645fd5e309fc176774dfcdc46db2b60d1a
Guillaume Gascon <guillaume.gascon@orange.fr>
2020-04-07 18:14:47 +0200

    New input and style modification
    Adding a new input password verification for the register page and make some sass modification like new title color and new color variable.

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import logo from '../img/logo.png'
 
 class RegisterForm extends Component {
+  constructor(){
+    super()
+    this.state = {
+      file: 'Choose a picture',
+      username: '',
+      password: '',
+      verification: '',
+      email: '',
+      avatar: '',
+    }
+  }
   render() {
     return (
       <div id='RegisterForm'>
@@ -18,13 +29,15 @@ class RegisterForm extends Component {
 
         <form>
           <label>Username</label>
-          <input type="text" name="username" placeholder="username"></input>
+          <input type="text" name="username" placeholder="username" id='username'></input>
           <label>Password</label>
-          <input type="password" name="password" placeholder="password"></input>
+          <input type="password" name="password" placeholder="password" id='password'></input>
+          <label>Repeat password</label>
+          <input type="password" name="verification" placeholder="repeat your password" id='verification'></input>
           <label>Email</label>
           <input type="email" name="email" placeholder="email"></input>
           <label>Avatar</label>
-          <label for="file" class="label-file">Choose a picture</label>
+          <label for="file" class="label-file">{this.state.file}</label>
           <input id='file' type="file" name="avatar"></input>
 
           <button>Register</button>
